8a86aaa3404d4a28923bc66e4386ceeda5cb2a85
root <root@beaglebone.localdomain>
2017-07-11 04:57:06 +0000

    put file in new directory so they could be committed.

---
+++ sandbox/headless_comm/headless_client.js
@@ -0,0 +1,6 @@
+var socket = require('socket.io-client')('http://localhost:3000');
+socket.on('connect', function(){});
+socket.on('event', function(data){});
+socket.emit('chat message', "ima beaglebone");
+socket.on('disconnect', function(){});
+console.log("done maybe perhaps?");
