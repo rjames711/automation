830e9b25feae3d175e328ac5c59dcad48d53a577
Rob <amesrobert2@gmail.com>
2017-07-25 23:11:02 +0000

    got local node custom module working (scratchapp)

--- sandbox/express_pug/scratchapp/app.js
+++ sandbox/express_pug/scratchapp/app.js
@@ -6,8 +6,17 @@ var port = process.env.PORT || 3000;
 server.listen(port);
 app.set('view engine', 'pug');
 app.set('views','./views'); //somehow allows me to use views directory with the pug file
+// first parameter is the mount point, second is the location in the file system
 app.use(express.static(__dirname + '/public')); //somehow allows me to use stylsheets folder
+
+//ok so apparently it like adding the directory to an envirement path where it checks for files.
 var count=0;
+
+
+var tools = require('./lib/tools.js');
+var value = tools.sum(10,20);
+console.log("Value: "+value);
+var items=tools.items;
 
 setInterval(count_up,1000);
 
@@ -16,26 +25,12 @@ function count_up(){
   count++;
 }
 
-var items = [
-    {code: 'A1', name: 'Count', description: '0'}, 
-    {code: 'A2', name: 'Soda2', description: 'a very long string which will make the tabler longer'}, 
-    {code: 'A3', name: 'Soda3', description: 'desc3'}, 
-    {code: 'A4', name: 'Soda4', description: 'desc4'}, 
-    {code: 'A5', name: 'Soda5', description: 'desc5'}, 
-    {code: 'A6', name: 'Soda6', description: 'desc6'}, 
-    {code: 'A7', name: 'Soda7', description: 'desc7'}, 
-    {code: 'A8', name: 'Soda8', description: 'desc8'}, 
-    {code: 'A9', name: 'Soda9', description: 'desc9'}, 
-    {code: 'A10', name: 'Soda10', description: 'desc10'}, 
-    {code: 'A11', name: 'Soda11', description: 'desc11'}
-];
-
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
 
-app.get('/first_template', function(req, res){
+app.get('/table', function(req, res){
    res.render('first_view', {title: 'autolab', items});
 });
 
