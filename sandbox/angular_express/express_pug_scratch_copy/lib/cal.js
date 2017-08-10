d8f0679727fa521b397eaca47e4c0adb00d5bf4d
Rob <amesrobert2@gmail.com>
2017-08-10 22:43:52 +0000

    Adding some of the angular stuff I'd worked on in cloud9

---
+++ sandbox/angular_express/express_pug_scratch_copy/lib/cal.js
@@ -0,0 +1,11 @@
+// File cal.js
+//simple example for importing modules
+
+module.exports = {
+    sum: function(a,b) {
+        return a+b
+    },
+    multiply: function(a,b) {
+        return a*b
+    }
+};
