83104e148feb218e238869b97a86c211b2bc2e8f
Rob <amesrobert2@gmail.com>
2017-08-10 19:25:23 +0000

    two_sensor_solenoid.js working for basic operation of fixture

--- sandbox/fixture_scripts/two_sensor_solenoid.js
+++ sandbox/fixture_scripts/two_sensor_solenoid.js
@@ -1,23 +1,24 @@
-//from examples in onoff repo
-//This one actually works
+//Tested with fixture dual sensor aircylinder fixture. Works correctly 
 "use strict";
 
 //Not sure I completely understand the syntaax here. Is GPIO a list?
 //Does't seem like onoff has ability to configure pullup/pulldown resistors
 //2,3,4 seem to be pullup by default, 20 seems to be pulldown by default
+//13 and 19 unfortunately seem to be pulldown by default 
 
-console.log('im a new running signal');
+console.log('script running');
 var Gpio = require('onoff').Gpio,
-    led = new Gpio(2, 'out'), //changed these two values
-    front_stop = new Gpio(3, 'in', 'both'),
-    end_stop = new Gpio(4, 'in', 'both');
+    led = new Gpio(26, 'out'), //changed these two values
+    front_stop = new Gpio(13, 'in', 'both'),
+    end_stop = new Gpio(19, 'in', 'both');
 
 
 led.writeSync(1);
 var state=0;
 var count=0;
 
-//function switch_solenoid(
+front_stop.watch(turn_on);
+end_stop.watch(turn_off);
 
 //So it seems the frontstop watches the value of its own pin 
 function turn_on(){
@@ -30,15 +31,11 @@ function turn_on(){
     }
 }
 
-front_stop.watch(turn_on);
+function turn_off(){
+state=0;
+led.writeSync(state);
+}
 
-end_stop.watch(function (err, value) {
-  if (err) {
-    throw err;
-  }
-    state=0;
-    led.writeSync(state);
-});
 
 
 process.on('SIGINT', function () {
