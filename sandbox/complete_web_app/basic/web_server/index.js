404d08a25e16131249447cf3961120f2c61d9bb2
Rob <amesrobert2@gmail.com>
2017-08-22 17:57:56 +0000

    added read file data method to index.js so it readsw in count.

--- sandbox/complete_web_app/basic/web_server/index.js
+++ sandbox/complete_web_app/basic/web_server/index.js
@@ -5,9 +5,24 @@ var port = process.env.PORT || 3000;
 var count=0;
 var speed=1000;
 var run_state=0;
+var fs =require('fs');
 const fixture = require('../fixture_objects/two_sensor_solenoid.js')
 var mach= new fixture();
 console.log('importing fixture object: ', mach.name);
+
+/**** Method for reading count. Don't mess with unless you mean to ****/
+function get_file_data(){    
+    function set_count(count) {mach.count=count} //Sets the count in the created object.
+    function read_count (callback){
+        fs.readFile( __dirname + '/count.txt', 'utf8' ,function(err,data){
+            if(err){throw err}
+            callback(data)
+            });}
+    read_count(set_count)
+}
+get_file_data();
+/****End Method for reading count. Don't mess with unless you mean to ****/
+
 
 mach.notify = function(){
   count= mach.count;
