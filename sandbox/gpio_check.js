7e4f18ad46a1bf46478cee63577add5fb43df6f7
Rob <amesrobert2@gmail.com>
2017-08-23 23:28:34 +0000

    added script in sandbox that check if gpio is accessible on hardware. works on pi need to check on cloud9. if works will implement into scripts so gpio imports dummy file for gpio control if not available for testing on other systems

---
+++ sandbox/gpio_check.js
@@ -0,0 +1,11 @@
+/**
+check if gpio are available for use on system by normal means 
+by checking if the sys/class/gpio directory exists
+*/
+var fs = require('fs');
+
+if (fs.existsSync('/sys/class/gpio')) {
+    console.log('gpio is acessible on this hardware');
+}
+else
+    console.log('gpio is acessible on this hardware');
