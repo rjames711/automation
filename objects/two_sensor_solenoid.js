4b4b2e35f4a80b7cdf91f3fa78cded30a70f7b6a
Rob <amesrobert2@gmail.com>
2017-08-24 00:54:23 +0000

    minor cleanup / commenting

--- objects/two_sensor_solenoid.js
+++ objects/two_sensor_solenoid.js
@@ -1,4 +1,6 @@
-// reworking to make in to reusable object
+/**
+ *Exports an object which controls an on / off actuator with two end stop sensors such as an air cylinder
+*/
 
 "use strict";
 console.log('starting');
@@ -8,7 +10,7 @@ function Two_Sensor_Solenoid() {
     self.path = __dirname;
     self.name = 'Two_Sensor_Solenoid'
     self.fs = require('fs');
-    if (self.fs.existsSync('/sys/class/gpio')) {
+    if (self.fs.existsSync('/sys/class/gpio')) { //Check if gpio are accessible on platform
         console.log('using real onoff module')
         var gpio_module = 'onoff';
     }
@@ -17,8 +19,7 @@ function Two_Sensor_Solenoid() {
         var gpio_module = './onoff';
     }
     self.Gpio = require(gpio_module).Gpio,
-        //self.Gpio = require('./onoff').Gpio, //for using dummy onoff file while testing on cloud9
-        self.led = new self.Gpio(26, 'out'), //changed these two values
+        self.output = new self.Gpio(26, 'out'), //changed these two values
         self.front_stop = new self.Gpio(13, 'in', 'both'),
         self.end_stop = new self.Gpio(19, 'in', 'both');
 
@@ -28,26 +29,20 @@ function Two_Sensor_Solenoid() {
     //the watch funciton below does not seem to be working in this module
     self.fs.watch(self.path + '/run_state.txt', self.get_run_state);
 
-    self.count = 55;
-    /*******paster in seperator********/
-
     self.notify = function() {} //trying an experiment here to see if I can redifine this in the importing module but still have it call from here.
 
 
-    //console.log('this',this);
-    //console.log('self',self);
-    //So it seems the frontstop watches the value of its own pin 
     self.turn_on = function() {
         if (self.state == 0) {
             self.state = 1;
             self.count++;
-            self.notify(); // experiment. See funciton comment.
+            self.notify(); // experiment. See function comment.
             self.fs.writeFile(self.path + '/count.txt', self.count + '\n', function(err) { if (err) throw err; }); //save count to file. Added error handling callback function to keep newer versoind of node from complaining.
             process.stdout.write("Running. Count: " + self.count + "        \r"); // update count in place
             if (self.count % 1000 == 0) //stop at 1000 cycle intervals
                 self.shut_off();
             if (self.running)
-                self.led.writeSync(self.state); //turn output on
+                self.output.writeSync(self.state); //turn output on
         }
     }
 
@@ -63,27 +58,14 @@ function Two_Sensor_Solenoid() {
         self.get_run_state();
     }
 
-    this.turn_off = function(led, state) {
+    this.turn_off = function(output, state) {
         self.state = 0;
-        //    console.log('led in turnoff', self.led);
-        //    console.log('self in turn off', self);
-        //    console.log('this in turn off', this);
-        self.led.writeSync(self.state);
+        self.output.writeSync(self.state);
     }
 
     self.front_stop.watch(self.turn_on);
     self.end_stop.watch(self.turn_off);
 
-    /**self.*****paster in seperator********/
-
-    /* Commmenting this function out. Using "this" the unexports don't work as they don't apear to refer to anything.
-        Using 'self' the program hangs and won't quit. need to revisit this. 
-
-        process.on('SIGINT', function() {
-            this.led.unexport();
-            this.front_stop.unexport();
-        });
-    */
     self.get_run_state = function() {
         console.log('in get runstate');
         self.fs.readFile(self.path + '/run_state.txt', function(err, data) {
@@ -98,11 +80,19 @@ function Two_Sensor_Solenoid() {
             }
             else {
                 self.running = 0;
-                self.led.writeSync(0); //turn output on
+                self.output.writeSync(0); //turn output on
             }
         });
     }
 
+    /* Commmenting this function out. Using "this" the unexports don't work as they don't apear to refer to anything.
+        Using 'self' the program hangs and won't quit. need to revisit this. 
+
+        process.on('SIGINT', function() {
+            this.output.unexport();
+            this.front_stop.unexport();
+        });
+    */
 }
 
 module.exports = Two_Sensor_Solenoid;
