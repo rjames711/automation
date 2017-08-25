8246421e08ca9b0cdeb41d019eeb90337f8b1da1
Rob <amesrobert2@gmail.com>
2017-08-25 00:44:14 +0000

    moved attribute that were in wrong section (public private). also renamed some function to self which were still this.

--- objects/two_sensor_solenoid.js
+++ objects/two_sensor_solenoid.js
@@ -1,22 +1,51 @@
 /**
  *Exports an object which controls an on / off actuator with two end stop sensors such as an air cylinder
-*/
+ */
 
 "use strict";
 console.log('starting');
 
 function Two_Sensor_Solenoid() {
     var self = this;
-    
-/**
-* Begin Private Variables
-*/
+
+    /**
+     * Begin Public Variables
+     */
     self.name = 'Two_Sensor_Solenoid'
     self.state = 0;
     self.count = 0;
     self.running = true;
     self.path = __dirname;
-    
+
+    self.shut_off = function() {
+        console.log('shutting down fixture');
+        //normally would just write a 0 to run_state.txt but fs.watch doesn't seem to work here
+        fs.writeFile(self.path + '/run_state.txt', 0 + '\n', function(err) { if (err) throw err; }); //write a zero to runstate file to stop running.
+        get_run_state(); //should be able delete if fs.watch is work which it seem to again
+    }
+
+    self.restart = function() {
+        fs.writeFile(self.path + '/run_state.txt', 1 + '\n', function(err) { if (err) throw err; }); //write a zero to runstate file to stop running.
+        get_run_state(); //should be able delete if fs.watch is work which it seem to again
+    }
+
+
+    /**
+     * Did this as an experiment and it worked first try. Purpose is have a function which can notify in 
+     * some other code where this objects is imported of an event in this object. I think what made this difficult
+     * as compared to other langueges is the weird asynchrounous patterns in js. Anyway the function is redifined in the
+     * code where this object is imported and when it is called here it runs the redefined code. Not sure its the best design pattern
+     * may want to look into other options such as eventEmitter, observer etc.
+     */
+    self.notify = function() {}
+
+
+
+
+    /**
+     * Begin Private attributes
+     */
+
     var turn_off = function() {
         self.state = 0;
         output.writeSync(self.state);
@@ -42,21 +71,17 @@ function Two_Sensor_Solenoid() {
         });
     }
 
-/**
- * Begin public attributes
-*/
+    var fs = require('fs');
 
-var fs = require('fs');
-    
     if (fs.existsSync('/sys/class/gpio')) { //Check if gpio are accessible on platform
         console.log('using real onoff module')
         var gpio_module = 'onoff';
     }
-    else{
+    else {
         console.log('using fake onoff module')
         var gpio_module = './onoff';
     }
-    
+
     var Gpio = require(gpio_module).Gpio,
         output = new Gpio(26, 'out'), //changed these two values
         front_stop = new Gpio(13, 'in', 'both'),
@@ -65,7 +90,7 @@ var fs = require('fs');
     var turn_on = function() {
         if (self.state == 0) {
             self.state = 1;
-            console.log('in turn on ', self.count );
+            console.log('in turn on ', self.count);
             self.count++;
             self.update_notify();
             fs.writeFile(self.path + '/count.txt', self.count + '\n', function(err) { if (err) throw err; }); //save count to file. Added error handling callback function to keep newer versoind of node from complaining.
@@ -77,35 +102,14 @@ var fs = require('fs');
         }
     }
 
-    this.shut_off = function() {
-        console.log('shutting down fixture');
-        //normally would just write a 0 to run_state.txt but fs.watch doesn't seem to work here
-        fs.writeFile(self.path + '/run_state.txt', 0 + '\n', function(err) { if (err) throw err; }); //write a zero to runstate file to stop running.
-        get_run_state(); //should be able delete if fs.watch is work which it seem to again
-    }
-
-    this.restart = function() {
-        fs.writeFile(self.path + '/run_state.txt', 1 + '\n', function(err) { if (err) throw err; }); //write a zero to runstate file to stop running.
-        get_run_state(); //should be able delete if fs.watch is work which it seem to again
-    }
 
 
-/**
- * Did this as an experiment and it worked first try. Purpose is have a function which can notify in 
- * some other code where this objects is imported of an event in this object. I think what made this difficult
- * as compared to other langueges is the weird asynchrounous patterns in js. Anyway the function is redifined in the
- * code where this object is imported and when it is called here it runs the redefined code. Not sure its the best design pattern
- * may want to look into other options such as eventEmitter, observer etc.
-*/
-    self.notify = function() {}
-    
 
-     
     //the watch funciton below does not seem to be working in this module. May want totry changing fs to var.fs
     fs.watch(self.path + '/run_state.txt', get_run_state);
     front_stop.watch(turn_on);
     end_stop.watch(turn_off);
-//note removed the unexport function since not being used. 
+    //note removed the unexport function since not being used. 
 }
 
 module.exports = Two_Sensor_Solenoid;
