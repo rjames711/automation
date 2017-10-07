498843e3e224ca6f5f53439f22c3b13ad4c621cc
Rob <amesrobert2@gmail.com>
2017-10-07 18:19:55 +0000

    implemented an array of movements to be executed in order

--- objects/components/step_motor.js
+++ objects/components/step_motor.js
@@ -7,72 +7,88 @@ function Step_Motor(direction_pin, stepper_pin) {
 
     var target = 0;
     var position = 0;
-    var direction=0;
-    var movements=[];   
+    var direction = 0;
+    var movements = [];
 
-    this.add_movement = function(new_position, speed , delay){
-        movments.push([new_position, speed, delay]);
+    this.add_movement = function(new_position, speed, delay) {
+        movements.push([new_position, speed, delay]);
     }
-     
-    this.step_to=function(new_position, spd){
+
+    this.begin_stepping = function() {
+        do_next_movement();
+    }
+
+    var do_next_movement = function() {
+        if(movements.length > 0){   
+        var next_move = movements.shift(),
+            next_pos = next_move[0],
+            spd = next_move[1],
+            delay = next_move[2];
+        setTimeout(function() { step_to(next_pos, spd) }, delay)
+    }
+    }
+
+    var step_to = function(new_position, spd) {
         set_target(new_position);
         start_motor(spd);
     }
-    
-    var start_motor = function (spd) {
-        if(get_steps_needed() > 0)
+
+    var start_motor = function(spd) {
+        if (get_steps_needed() > 0)
             set_direction(1);
         else
             set_direction(0);
         step_pin.pwmFrequency(spd);
         step_pin.pwmWrite(100);
     }
-    
-    var stop_motor = function(){
+
+    var stop_motor = function() {
         step_pin.digitalWrite(0);
     }
 
     var set_target = function(steps) {
         target = steps;
     }
-    
-    var set_direction= function(dir){
-        this.direction=dir;
+
+    var set_direction = function(dir) {
+        direction = dir;
         dir_pin.digitalWrite(dir);
     }
-    
-    var took_step= function(){
+
+    var took_step = function() {
         if (direction)
-            position--;
-        else
             position++;
-            
-            
-        if(get_steps_needed() == 0){
+        else
+            position--;
+            console.log(position, ' ', get_steps_needed());
+        if (get_steps_needed() == 0) {
             stop_motor();
-            console.log(position,' ', get_steps_needed() );
+            console.log(position, ' ', get_steps_needed());
+            do_next_movement();
         }
     }
-    
-    var get_steps_needed = function(){
-        return target-position;
+
+    var get_steps_needed = function() {
+        return target - position;
     }
-    
+
     step_pin.on('alert', function(level, tick) {
-        if (level == 1) 
+        if (level == 1)
             took_step();
     });
 
-setTimeout(function(){
-console.log(position);
-},1000);
+    setTimeout(function() {
+        console.log(position);
+    }, 1000);
 
 }
 
 var stepper1 = new Step_Motor(17, 27);
 
-stepper1.step_to(100, 300);
-//stepper1.step_to(100,700);
-//stepper1.step_to(360,300);
-//stepper1.step_to(-180,700);
-//stepper1.step_to(0,300);
+stepper1.add_movement(50, 300, 0);
+stepper1.add_movement(100, 300, 500);
+stepper1.add_movement(200, 300, 250);
+stepper1.add_movement(0, 700, 250);
+stepper1.add_movement(-300, 700, 250);
+stepper1.add_movement(0, 700, 250);
+stepper1.begin_stepping();
