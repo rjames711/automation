e2907d97ee8f07276393d48b129f52af33c7eba2
Rob <amesrobert2@gmail.com>
2017-10-07 17:36:50 +0000

    Starting objectifying step motor component

--- objects/components/step_motor.js
+++ objects/components/step_motor.js
@@ -1,110 +1,78 @@
-function step_motor(dir_pin, step_pin, steps_per_rev) {
+function Step_Motor(direction_pin, stepper_pin) {
+
     var pigpio = require('pigpio');
     var Gpio = pigpio.Gpio;
-    var sensor = new Gpio(19, {
-        mode: Gpio.INPUT,
-        pullUpDown: Gpio.PUD_DOWN,
-        edge: Gpio.RISING_EDGE,
-    });
-    var dir_pin = new Gpio(17, { mode: Gpio.OUTPUT });
-    var step_pin = new Gpio(27, { mode: Gpio.OUTPUT, alert: true });
-    var state = true;
-    var spd = 700;
-    var dir = true;
-    var steps = 0;
-    var steps_needed = 0;
-    var steps_per_rev = 200 * 15.3;
-    var current_pos = 0;
-    var dest_pos = 60;
-    var end_delay = 500;
-    var homed_in = false;
-    var new_home_pass = true;
-    var offset = dest_pos - 180;
-    var cycling = true;
+    var dir_pin = new Gpio(direction_pin, { mode: Gpio.OUTPUT });
+    var step_pin = new Gpio(stepper_pin, { mode: Gpio.OUTPUT, alert: true });
 
-    //Set the direction and desired number of steps to next position and begins stepping
-    function go_to_dest(dest_pos, spd) {
-        var displacement = dest_pos - current_pos;
-        console.log(dest_pos, current_pos, displacement);
-        steps_needed = Math.abs(degrees_to_steps(displacement));
-        if ((displacement) > 0) {
-            change_dir(1);
-            new_home_pass = true;
-        }
+    var target = 0;
+    var position = 0;
+    var direction=0;
+    var movements=[];   
+
+    this.add_movement = function(new_position, speed , delay){
+        movments.push([new_position, speed, delay]);
+    }
+     
+    this.step_to=function(new_position, spd){
+        set_target(new_position);
+        start_motor(spd);
+    }
+    
+    var start_motor = function (spd) {
+        if(get_steps_needed() > 0)
+            set_direction(1);
         else
-            change_dir(0);
-        run_motor(spd);
-    }
-
-    //Counts steps by catching alerts when step pin is set high. 
-    //stops motor and cycles when steps = steps_needed
-    //Sets angular position
-    step_pin.on('alert', function(level, tick) {
-        if (level == 1) {
-            steps++;
-            if (dir)
-                current_pos += steps_to_degrees(1);
-            else
-                current_pos -= steps_to_degrees(1);
-            if (steps >= steps_needed) {
-                steps = 0;
-                stop_motor();
-                if (cycling)
-                    setTimeout(cycle, end_delay);
-            }
-            if (steps % 100 == 0)
-                console.log(steps, ' ', steps_needed, ' ', current_pos);
-        }
-    });
-
-    //Both conversion functions tested ok
-    function steps_to_degrees(steps) {
-        var revs = steps / steps_per_rev;
-        return revs * 360;
-    }
-
-    function degrees_to_steps(degs) {
-        var revs = degs / 360;
-        return revs * steps_per_rev;
-    }
-
-    function run_motor(spd) {
+            set_direction(0);
         step_pin.pwmFrequency(spd);
         step_pin.pwmWrite(100);
     }
-
-    function stop_motor() {
+    
+    var stop_motor = function(){
         step_pin.digitalWrite(0);
-        steps_needed = 0;
     }
 
-    function change_dir(new_dir) {
-        dir = new_dir;
+    var set_target = function(steps) {
+        target = steps;
+    }
+    
+    var set_direction= function(dir){
+        this.direction=dir;
         dir_pin.digitalWrite(dir);
     }
-
-    sensor.on('interrupt', sensor_handler);
     
-    function sensor_handler(level){
+    var took_step= function(){
+        if (direction)
+            position--;
+        else
+            position++;
+            
+            
+        if(get_steps_needed() == 0){
+            stop_motor();
+            console.log(position,' ', get_steps_needed() );
+        }
     }
-
-    process.on('SIGINT', function() {
-        console.log('shutting down');
-        cycling = false;
-        stop_motor();
-
-        setTimeout(function() {
-            go_to_dest(offset, 500);
-        }, 500);
-
-        setTimeout(cleanup, 3000);
+    
+    var get_steps_needed = function(){
+        return target-position;
+    }
+    
+    step_pin.on('alert', function(level, tick) {
+        if (level == 1) 
+            took_step();
     });
 
-    function cleanup() {
-        step_pin.digitalWrite(0);
-        dir_pin.digitalWrite(0);
-        pigpio.terminate(); // pigpio C library terminated here
-        console.log('Terminating...');
-        process.exit();
-    }
+setTimeout(function(){
+console.log(position);
+},1000);
+
 }
+
+var stepper1 = new Step_Motor(17, 27);
+
+stepper1.step_to(100, 300);
+//stepper1.step_to(100,700);
+//stepper1.step_to(360,300);
+//stepper1.step_to(-180,700);
+//stepper1.step_to(0,300);
