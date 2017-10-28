c6e74e831db225f54da72ec52025f0779adb465d
Rob <amesrobert2@gmail.com>
2017-10-28 20:31:03 +0000

    added pin mapping file for the asme valve tester pcb (rev1)

---
+++ objects/fixtures/asme_valve_tester/pin_mappings.js
@@ -0,0 +1,28 @@
+
+var sda_1=2; //(sda1_gpio2)
+var scl_1=3; //(scl1_gpio3)
+var wire_1=4; //(glck gpio4) 1 wire sensors
+var sol_4=5;
+var sol_3=6;
+var ce_1=7; 
+var spi_ce0=8;
+var spi_miso=9;
+var spi_mosi=10;
+var spi_sclk=11;
+var step_2=12;
+var sol_2=13;
+var tx=14;
+var rx=15;
+var dir_2=16;
+var opto_1=17;
+var step_4=18;
+var sol_1=19;
+var step_1=20;
+var dir_1=21;
+var opto_3=22;
+var dir_4=23;
+var step_3=24;
+var dir_3=25;
+var opto_4=26;
+var opto_2=27;
+
