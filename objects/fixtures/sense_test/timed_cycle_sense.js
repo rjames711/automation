059e4fb600f6b33462c2f1edb55c26738ca02519
Rob <amesrobert2@gmail.com>
2017-12-07 17:02:50 +0000

    added logging function to sensor faucet cycling code

--- objects/fixtures/sense_test/timed_cycle_sense.js
+++ objects/fixtures/sense_test/timed_cycle_sense.js
@@ -14,7 +14,7 @@ function Timed_Cycle_Solenoid() {
     self.name = 'Two_Sensor_Solenoid'
     self.state = false;
     self.count = 0;
-    self.activations=0;
+    self.activations = 0;
     self.running = true;
     self.path = __dirname;
     self.interval = 3500;
@@ -22,7 +22,7 @@ function Timed_Cycle_Solenoid() {
     self.outercommand = { 'SHUT OFF': "itsa button", 'change count': 'itsa number input' };
 
     self.shut_off = function() {
-        self.state=0;
+        self.state = 0;
         solenoid.switch(0);
         console.log('shutting down fixture');
         fs.writeFile(self.path + '/run_state.txt', 0 + '\n', function(err) { if (err) throw err; }); //write a zero to runstate file to stop running.
@@ -50,14 +50,18 @@ function Timed_Cycle_Solenoid() {
 
     var toggle = function() {
         if (self.running) {
-            fs.writeFile(self.path + '/count.txt', self.count + '\n', function(err) { if (err) throw err; }); 
-           process.stdout.write("Running. Count: " + self.count +' activations '+self.activations+ "        \r"); // update count in place
+            fs.writeFile(self.path + '/count.txt', self.count + '\n', function(err) { if (err) throw err; });
+            fs.writeFile(self.path + '/log.txt', 'count: ' +
+                self.count + ' time: ' + (new Date).getTime() + '\n',
+                function(err) { if (err) throw err; });
+            (new Date).getTime();
+            process.stdout.write("Running. Count: " + self.count + ' activations ' + self.activations + "        \r"); // update count in place
             self.state = !self.state;
             solenoid.switch(Number(self.state));
-            if (self.state){
+            if (self.state) {
                 self.count++;
                 self.notify();
-                
+
             }
         }
     }
@@ -97,34 +101,40 @@ function Timed_Cycle_Solenoid() {
     const binary_pin = require('../../components/binary_pin.js');
     var solenoid = new binary_pin(26, 'out');
 
-var Gpio = require('onoff').Gpio,
-  button = new Gpio(13, 'in', 'both');
+    var Gpio = require('onoff').Gpio,
+        button = new Gpio(13, 'in', 'both');
 
-button.watch(function(err, value) {
-    self.activations+=.5;
-//    console.log(self.count, 'cycles', self.activations, 'activations');
-});
+    button.watch(function(err, value) {
+        self.activations += .5;
+        if (self.activations % 1 == 0) {
+            fs.writeFile(self.path + '/log.txt', 'activations: ' +
+                self.activations + ' time: ' + (new Date).getTime() + '\n',
+                function(err) { if (err) throw err; });
+        }
+        //    console.log(self.count, 'cycles', self.activations, 'activations');
+    });
 
 
-/**** Method for reading count. Don't mess with unless you mean to ****/
-function get_file_data(){    
-    function set_count(count) {self.count=count} //Sets the count in the created object.
-    function read_count (callback){
-        fs.readFile( self.path + '/count.txt', 'utf8' ,function(err,data){
-            if(err){throw err}
-            callback(data)
-            });}
-    read_count(set_count)
-}
-get_file_data();
-/****End Method for reading count. Don't mess with unless you mean to ****/
+    /**** Method for reading count. Don't mess with unless you mean to ****/
+    function get_file_data() {
+        function set_count(count) { self.count = count } //Sets the count in the created object.
+        function read_count(callback) {
+            fs.readFile(self.path + '/count.txt', 'utf8', function(err, data) {
+                if (err) { throw err }
+                callback(data)
+            });
+        }
+        read_count(set_count)
+    }
+    get_file_data();
+    /****End Method for reading count. Don't mess with unless you mean to ****/
 
     //the watch funciton below does not seem to be working in this module. May want totry changing fs to var.fs
     fs.watch(self.path + '/run_state.txt', get_run_state);
-    setInterval(toggle,self.interval);
+    setInterval(toggle, self.interval);
 
 }
 
 module.exports = Timed_Cycle_Solenoid;
 
-var test= new Timed_Cycle_Solenoid();
+var test = new Timed_Cycle_Solenoid();
