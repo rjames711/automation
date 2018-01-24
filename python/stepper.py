4e75589ad9849acfaba724193e10e40b517e8e41
Rob <amesrobert2@gmail.com>
2018-01-24 15:32:11 +0000

    Motor now resets position when it passes sensor to guard against losing steps. Needs to be refined. Also TODO the motor responds correctly to go home with cntrl-c unless its already in the home pos in which case it goes elsewhere. Need to fix and refine.

--- python/stepper.py
+++ python/stepper.py
@@ -16,14 +16,17 @@ class Stepper:
         self.set_speed(1) #set default low speed
         self.target=0
         self.pi.callback(step_pin, pigpio.RISING_EDGE, self.on_step)
+        self.pi.callback(sense_pin, pigpio.RISING_EDGE, self.on_sense)
         self.pi.write(dir_pin,0) # set default direction
         self.moves= []
         self.cycle=False
         self.cycle_moves=[]
         self.running=False
         self.count=int(open('count.txt').read())
+        self.pos_error=0
         print 'Loaded count: ',self.count
-
+        self.update_line()
+        self.new_home_pass=False
     #Param is a integer 1 - 9 which corresponds to a frequency (speed) 
     #Function sets the pwm frquency. Available frequencies based on pigpio docs
     def set_speed(self, spd):  
@@ -48,10 +51,28 @@ class Stepper:
             self.position-=1
         if self.position==self.target:
             self.run_next_move()
+    
+    #May need fixing if home in other direction (switch read values)
+    # Right now is set to rehome position since this program is losing steps somewhere.
+    def on_sense(self,gpio,level,tick):
+        if self.new_home_pass and self.pi.read(self.dir_pin)==0:
+            self.pos_error = self.position
+            self.new_home_pass=False
+            self.position=0 #reset position to zero (to use if steps are missing)
+        elif self.pi.read(self.dir_pin)==1:
+            self.new_home_pass=True
 
+    
+    def update_line(self):
+	sys.stdout.write('Count: '  + str(self.count)+' Pos Error: '+ str(self.pos_error) + "\r")
+        sys.stdout.flush()
+    
     def log_count(self):
         log=open('count.txt','w')
         log.write(str(self.count))
+
+    
+    
 
     def run_next_move(self):
         self.running=True
@@ -59,8 +80,7 @@ class Stepper:
             self.moves=list(self.cycle_moves) #if cycle is set, cycle_move will have moves
             self.count+=1 #if no moves left that a cycles has completed
             self.log_count()
-            sys.stdout.write('Count: '  + str(self.count)+ "\r")
-            sys.stdout.flush()
+            self.update_line()
         if self.moves:
             move=self.moves.pop(0)
             self.step_deg_pos(move[0], move[1], move[2])
@@ -102,43 +122,47 @@ class Stepper:
     def clear_moves(self):
         self.moves=[]
         self.cycle_moves=[]
+if __name__=='__main__':
+    #Begin Script 
+    start_pos=20
+    motor=Stepper(20, 21, 17, 3060)
+    motor.find_home(1,0)
+    motor.add_move(start_pos-100,3,.5)
+    motor.add_move(start_pos,3,.5)
+    motor.set_cycle(1)
+    motor.run_next_move()
 
-    
-top_pos=-27
-motor=Stepper(20, 21, 17, 3060)
-motor.find_home(1,1)
-motor.add_move(top_pos+90,3,.5)
-motor.add_move(top_pos,4,1)
-motor.set_cycle(1)
-motor.run_next_move()
-while True:
-    try:
-        time.sleep(1)
-    except KeyboardInterrupt:
-        print('Homing motor')
-        motor.stop_motor()
-        motor.clear_moves()
-        motor.add_move(top_pos,2,0)
-        motor.run_next_move()
-        while motor.running:
-            time.sleep(.1)
-        print('exiting')
-        sys.exit(0)
-motor.stop_motor()
-motor.pi.stop()
+    #Keep running until cntrl-c then go home
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            print('Homing motor')
+            motor.stop_motor()
+            motor.clear_moves()
+            #if abs(motor.position -(start_pos/360)*3060) > 100: # only try to move home if position is off
+            if True:
+                motor.add_move(start_pos,2,0)
+                motor.run_next_move()
+                while motor.running:
+                    time.sleep(.1)
+            print('exiting')
+            sys.exit(0)
+    motor.stop_motor()
+    motor.pi.stop()
 
 
-'''
-motor.set_direction(0)
-motor.set_speed(3)
-motor.start_motor()
-time.sleep(3)
-motor.set_direction(1)
-motor.stop_motor()
-time.sleep(1)
-motor.start_motor()
-motor.set_speed(5)
-time.sleep(6)
-print('timeouit')
-motor.stop_motor()
-'''
+    '''
+    motor.set_direction(0)
+    motor.set_speed(3)
+    motor.start_motor()
+    time.sleep(3)
+    motor.set_direction(1)
+    motor.stop_motor()
+    time.sleep(1)
+    motor.start_motor()
+    motor.set_speed(5)
+    time.sleep(6)
+    print('timeouit')
+    motor.stop_motor()
+    '''
