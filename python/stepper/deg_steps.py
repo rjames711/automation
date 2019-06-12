0ee1ff67a596ea0b3f7ecfe417deb91c22698e4c
lab <rames@speakman.com>
2019-06-12 11:27:18 -0400

    save before wiping

--- python/stepper/deg_steps.py
+++ python/stepper/deg_steps.py
@@ -1,9 +1,11 @@
 # takes a cmd arg of degrees to step and steps
 import stepper, sys, time
-steps_per_rev= 200 * 27
+#steps_per_rev= 200 * 27
+steps_per_rev= 200*4
 motor = stepper.Stepper(20,21,16,steps_per_rev)
 degs=int(sys.argv[1])
 print 'degrees: ' , degs
-motor.add_move(degs, 6,0)
+motor.add_move(degs, 1,0)
 motor.run_next_move()
-time.sleep(.5 + abs(degs)*steps_per_rev/(3060*50)) #delay proportionate to step length. Not precise here.
+#time.sleep(.5 + abs(degs)*steps_per_rev/(3060*50)) #delay proportionate to step length. Not precise here.
+time.sleep(3)
