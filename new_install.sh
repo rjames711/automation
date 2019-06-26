502df9543750d8d75cf3b00198d4201ecca910dd
Rob Ames <rames@speakman.com>
2019-06-26 15:24:31 +0100

    Added got config to new install csript

---
+++ new_install.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt upgrade
+sudo apt --assume-yes install git
+sudo apt --assume-yes install tmux
+sudo apt --assume-yes install fish
+sudo apt --assume-yes install python-pip
+sudo apt --assume-yes install python3-pip
+sudo apt --assume-yes install vim
+sudo apt --assume-yes install ipython
+sudo apt --assume-yes install ipython3
+sudo apt --assume-yes install pigpio python-pigpio python3-pigpio
+ 
+git config --global user.email "rames@speakman.com"
+git config --global user.name "Rob Ames"
