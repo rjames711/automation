aa6a16e505cc0d982e3b78efcf78c9c74dc63665
Rob Ames <rames@speakman.com>
2019-08-05 17:44:56 +0100

    added small flask web app for managing tests

---
+++ flaskweb/app.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+# coding: utf-8
+from flask import Flask, Blueprint, flash, g, redirect, render_template, request, url_for, session
+import os
+app = Flask(__name__)
+
+tests = []
+class TestObj:
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path+self.name
+        self.countfile = self.path+"/count.txt"
+        self.count = self.read_count()
+        self.run_file = self.path + "/run.txt"
+        self.running = self.get_run_state() 
+    
+    def read_count(self):
+        return int(open(self.countfile).read())
+
+    def get_run_state(self):
+        return int(open(self.run_file).read())
+    
+    def toggle_run(self):
+        newstate = str(int(not bool(self.get_run_state())))
+        with open(self.run_file,'w') as rf:
+            rf.write(newstate)
+            
+        
+
+
+
+
+
+@app.route('/')
+def entry_point():
+    return 'Hello World!'
+
+@app.route('/app', methods=('GET','POST'))
+def main():
+    if request.method == 'POST':
+        print('got request')
+        test = list(dict(request.form))[0]
+        testobj = None
+        for t in tests:
+            if t.name == test:
+                testobj = t
+                testobj.toggle_run()
+        print ('test',testobj)
+        
+    global tests    
+    tests = generate_tests()
+    return render_template('index.html',tests=tests)
+
+def generate_tests():
+    a_dir = "/home/pi/current_tests/"
+    tests = [name for name in os.listdir(a_dir) if os.path.isdir(os.path.join(a_dir, name))]
+    testobjs= [ TestObj(x,a_dir) for x in tests]
+    return testobjs
+
+
+
+if __name__ == '__main__':
+    print(generate_tests())
+    app.run(host='0.0.0.0', debug=True)
+
--- python/stepper/step_script.py
+++ python/stepper/step_script.py
@@ -1,9 +1,10 @@
 import stepper, time, sys
-motor=stepper.Stepper(20, 21, 17, 200*27)
+motor=stepper.Stepper(20, 21, 17, 200*8)
 
-spd = 5
-
-end = 180
+with open('run.txt','w') as r:
+    r.write('1')
+spd = 1
+end = 47
 stop1 = 10
 stop2 = 20
 stop3 = end-10
@@ -19,14 +20,15 @@ motor.add_move(0, spd,0)
 print motor.position, motor.target
 motor.set_cycle(1)
 motor.run_next_move()
-while motor.position!=motor.target or motor.moves:
+while True: 
     #print motor.position, motor.target
     try:
-        time.sleep(.1)
-        if motor.count % 250 == 0:
-            motor.stop_motor()
-            while True:
-                time.sleep(1)
+        time.sleep(.5)
+        with open('run.txt','r') as r:
+            if motor.count % 500000 == 0 or not int(r.read()):
+                motor.stop_motor()
+                print 'Stopping motor'
+                sys.exit(0) 
     except KeyboardInterrupt:
         print('Homing motor')
         motor.stop_motor()
@@ -37,7 +39,10 @@ while motor.position!=motor.target or motor.moves:
             motor.run_next_move()
             while motor.running:
                 time.sleep(.1)
-        print('exiting')
+        print('exiting controlled')
         sys.exit(0)
+    except:
+        print 'Caught some random erro'
 motor.stop_motor()
 motor.pi.stop()
+print 'Exiting without exception end of script'
